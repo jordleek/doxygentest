01f2d706f60f0bd29737dddf45fc2220e6cc2bc5
jordleek <jordleek2000@gmail.com>
2024-01-23 08:49:23 +0100

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,16 @@
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+}
+
+/// <summary>
+/// This property always returns a value &lt; 1.
+/// </summary>
+public class player()
+{
+  
+}
